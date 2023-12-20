d33868556d1a9c2644f3a9f1d037343abe4f668a
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2023-12-20 16:52:25 +0530

    added checking options functionality

--- src/App.java
+++ src/App.java
@@ -1,4 +1,13 @@
 public class App {
+    static int NO_PLAY = 0;
+    static int LADDER = 1;
+    static int SNAKE = 2;
+
+    public static int checkOption() {
+        int option = (int) (Math.random() * 3);
+
+        return option;
+    }
 
     public static int rollDice() {
         int roll = (int) (Math.random() * 6) + 1;
@@ -12,8 +21,23 @@ public class App {
         int position = 0;
         System.out.println("Starting position of player: " + position);
 
+        int currOption = checkOption();
+
+        if (currOption == NO_PLAY) {
+            System.out.println("Your current option is no play");
+            return;
+        }
+
+        System.out.println("Your current option is " + (currOption == LADDER ? "ladder" : "snake"));
+
         int currTurn = rollDice();
         System.out.println("Current turn: " + currTurn);
 
+        if (currOption == LADDER)
+            position += currTurn;
+        else
+            position -= currTurn;
+
+        System.out.println("Your new position is " + position);
     }
 }
