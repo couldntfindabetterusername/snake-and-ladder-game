0b0eec89266e91e3d5a68f94df2b49c240cc974a
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2023-12-20 16:25:51 +0530

    roll dice functionality

--- src/App.java
+++ src/App.java
@@ -1,8 +1,19 @@
 public class App {
+
+    public static int rollDice() {
+        int roll = (int) (Math.random() * 6) + 1;
+
+        return roll;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to snake and ladders game");
 
-        int startPos = 0;
-        System.out.println("Starting position of player: " + startPos);
+        int position = 0;
+        System.out.println("Starting position of player: " + position);
+
+        int currTurn = rollDice();
+        System.out.println("Current turn: " + currTurn);
+
     }
 }
