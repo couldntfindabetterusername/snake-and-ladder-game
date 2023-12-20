e793432c4e60474d0b8793cd27c7e4388d0ebb2a
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2023-12-20 14:47:33 +0530

    start position added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to snake and ladders game");
+
+        int startPos = 0;
+        System.out.println("Starting position of player: " + startPos);
     }
 }
